aee757302fecefae967a2232cb043db4c8dc609f
Sauli Tähkäpää <saulis@reaktor.fi>
2023-08-15 08:46:20 +0300

    disable minification for production builds to show code in editor properly

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return {
+      ...config,
+      optimization: { ...config.optimization, minimize: false },
+    };
+  },
+};
